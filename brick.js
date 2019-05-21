9db4798322b3d2a24e58742d83cab83fbf46c930
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-05-21 11:30:47 +0800

    Fix rotating not work on back face
    Fix #66

--- brick.js
+++ brick.js
@@ -176,7 +176,10 @@ class Brick {
     let newFaceNormalVector = new THREE.Vector3(this.faceX, this.faceY, this.faceZ).applyQuaternion(this.renderObject.quaternion).round();
     let rotaryAxisCross = this.faceNormalVector.clone().cross(newFaceNormalVector);
 
-    if (this.rotaryAxis.equals(new THREE.Vector3(0, 0, -1))) {
+    if (this.rotaryAxis.equals(new THREE.Vector3(0, 0, -1)) || this.rotaryAxis.equals(new THREE.Vector3(0, 0, 1))) {
+      if (this.rotaryAxis.equals(new THREE.Vector3(0, 0, 1))) {
+        angle = 4 - angle;
+      }
       if (this.rotaryAxis.equals(rotaryAxisCross)) {
         angle = 4 - angle;
       }
@@ -185,7 +188,10 @@ class Brick {
       return;
     }
 
-    if (this.rotaryAxis.equals(new THREE.Vector3(1, 0, 0))) {
+    if (this.rotaryAxis.equals(new THREE.Vector3(1, 0, 0)) || this.rotaryAxis.equals(new THREE.Vector3(-1, 0, 0))) {
+      if (this.rotaryAxis.equals(new THREE.Vector3(-1, 0, 0))) {
+        angle = 4 - angle;
+      }
       if (this.rotaryAxis.clone().negate().equals(rotaryAxisCross)) {
         angle = 4 - angle;
       }
@@ -194,7 +200,10 @@ class Brick {
       return;
     }
 
-    if (this.rotaryAxis.equals(new THREE.Vector3(0, 1, 0))) {
+    if (this.rotaryAxis.equals(new THREE.Vector3(0, 1, 0)) || this.rotaryAxis.equals(new THREE.Vector3(0, -1, 0))) {
+      if (this.rotaryAxis.equals(new THREE.Vector3(0, -1, 0))) {
+        angle = 4 - angle;
+      }
       if (this.rotaryAxis.clone().negate().equals(rotaryAxisCross)) {
         angle = 4 - angle;
       }
